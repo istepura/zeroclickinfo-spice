25d66c69c597cf8856badd827dfa8ca3d6db14b3
Igor Stepura <igor.stepura@gmail.com>
2013-07-09 23:38:50 +0900

    Add more details

--- share/spice/pack_track/usps/usps.js
+++ share/spice/pack_track/usps/usps.js
@@ -3,32 +3,30 @@ function ddg_spice_pack_track_usps(api_result){
     if (!api_result) return;
 
     var xmlresp = $( $.parseXML(api_result) );
-    var err= xmlresp.find('Error') ;
-    if (err.length > 0) return;
 
-
-    var response = xmlresp.find('TrackResponse');
-    if (!response) return;
+    if (xmlresp.find('Error').length > 0) return;
+    if (xmlresp.find('TrackResponse').length <= 0) return;
 
     var jsondata = []
-    xmlresp.find('TrackInfo').each(function( ) {
-        $(this).find( "TrackSummary" ).each( buildrow);
+    var buildrow = (function(output){
+            return function (idx, elem){
+                    var fn_ = (function (){ return $(this).text()});
+                    output.push({
+                       status : $(elem).find('Event').text(),
+                       date : $(elem).find( 'EventDate, EventTime').map(fn_).get().join(", "),
+                       place : $(elem).find('EventCity, EventState, EventZIPCode').map(fn_).get().join(", ")
+                    });
+                }
+            })(jsondata);
+    xmlresp.find( "TrackSummary, TrackDetail" ).each( buildrow );
 
-        }
-    );
     Spice.render({
         data             : { rows : jsondata},
-        header1          : ' (Today in History)',
+        header1          :  $(xmlresp.find("TrackInfo")).attr("ID"), 
         source_url       : "usps.com",
         source_name      : 'USPS',
         template_normal  : 'usps',
         force_big_header : false 
     });
-    function buildrow(){
-        jsondata.push({status : $(this).find('Event').text(),
-                   zip : $(this).find('EventZIPCode').text()});
-    }
-        
 }
-
 
