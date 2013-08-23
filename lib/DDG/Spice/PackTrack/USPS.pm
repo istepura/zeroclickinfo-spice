84de8755d261eb842b54a428a30cfdd7027b8dd7
Igor Stepura <igor.stepura@gmail.com>
2013-08-23 13:36:30 +0900

    Add more comments

--- lib/DDG/Spice/PackTrack/USPS.pm
+++ lib/DDG/Spice/PackTrack/USPS.pm
@@ -13,6 +13,13 @@ spice wrap_string_callback => 1;
 spice is_cached => 0;
 
 handle query_nowhitespace => sub {
+    # Don't do any matching ID validation here.
+    # It looks like old POSTNET mod 10 verification algorithm
+    # is no longer valid after Intelligent Mail barcode introduction.
+    # http://en.wikipedia.org/wiki/POSTNET#cite_note-3
+    # So, just query USPS server with what we've got from user.
+    # If it's valid, we'll get meaningful response XML, otherwise server
+    # will report an error
     my ($id) = $_;
 
     return $id if $id;
